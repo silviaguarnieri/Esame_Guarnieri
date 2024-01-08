3fe6c5a504a82652f45322cd45970da8e3335372
Silvia <502silvia@gmail.com>
2024-01-08 11:07:46 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const menu = document.querySelector('#menu')
+const toggle = document.querySelector('#toggle')
+
+const toggleMunu = () => {
+toggle.classList.toggle('is-active')
+menu.classList.toggle('active')
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    toggle.addEventListener
+})
+
+const setup = () => {
+    console.log('setup')
+
+    menuButton.addEventListener('click', toggleMenu)
+}
+
+document.addEventListener('DOMContentLoaded', setup)
